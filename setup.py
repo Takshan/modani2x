8d4d6cbf9810ca9884390786ecc40b472e028d94
Takshan <mrbrahma.rahul@gmail.com>
2021-04-12 09:34:19 +0900

    merged main.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ classifiers = [
 
 setup(
     name='modani2x',
-    version='0.0.2.5',
+    version='0.0.2.6',
     description='A simple basic script to test package publications.',
     long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
     url='',  
